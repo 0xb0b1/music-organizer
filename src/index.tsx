63beaa3e32850abe66215452410af355908f2f06
Paulo Vicente <vcente82@gmail.com>
2021-09-24 12:14:10 -0300

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,6 @@ import App from "./App";
 
 import "./styles/index.scss";
 
-import "./styles/custom-slide-item.scss";
-
-// swiper styling
-import "swiper/swiper.min.css";
-import "swiper/components/navigation/navigation.min.css";
-import "swiper/components/pagination/pagination.min.css";
-import "swiper/components/scrollbar/scrollbar.min.css";
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
