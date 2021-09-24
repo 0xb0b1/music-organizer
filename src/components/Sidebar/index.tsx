5f64ceb2f523188527badc69b8adc64bbfb2e876
Paulo Vicente <vcente82@gmail.com>
2021-09-24 11:14:39 -0300

    add css tooltip

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,36 +7,39 @@ import { Link } from "react-router-dom";
 
 export const Sidebar = () => {
   return (
-    <aside className="hidden md:flex px-2 py-2 w-80 bg-gradient-to-t from-gray-800 h-home">
+    <aside className="flex px-2 py-2 w-auto bg-gradient-to-t from-gray-800 h-home">
       <ul className="flex flex-col gap-2 text-white py-4">
         <li>
           <Link
-            className="bg-gray-800 w-auto h-12 flex items-center gap-2 px-4 rounded-3xl cursor-pointer"
+            className="has-tooltip bg-gray-800 w-auto h-12 flex items-center gap-2 px-4 rounded-3xl cursor-pointer"
             // activeStyle={{ color: "red" }}
             to="/"
           >
             <RiHome2Fill size={22} />
-            <span className="font-semibold text-sm text-gray-200">Home</span>
+            {/* <span className="hidden md:font-semibold text-sm text-gray-200"> */}
+            <span className="tooltip bg-gray-700 rounded-xl p-4 font-semibold text-sm text-gray-200">
+              Home
+            </span>
           </Link>
         </li>
         <li>
           <Link
-            className="bg-gray-800 w-auto h-12 flex items-center gap-2 px-4 rounded-3xl cursor-pointer"
+            className="has-tooltip bg-gray-800 w-auto h-12 flex items-center gap-2 px-4 rounded-3xl cursor-pointer"
             to="/myplaylists"
           >
             <RiPlayList2Fill size={22} />
-            <span className="font-semibold text-sm text-gray-200">
+            <span className="tooltip bg-gray-700 rounded-xl p-4 font-semibold text-sm text-gray-200">
               My Playlists
             </span>
           </Link>
         </li>
         <li>
           <Link
-            className="bg-gray-800 w-auto h-12 flex items-center gap-2 px-4 rounded-3xl cursor-pointer"
+            className="has-tooltip bg-gray-800 w-auto h-12 flex items-center gap-2 px-4 rounded-3xl cursor-pointer"
             to="/categories"
           >
             <RiFolderMusicFill size={22} />
-            <span className="font-semibold text-sm text-gray-200">
+            <span className="tooltip bg-gray-700 rounded-xl p-4 font-semibold text-sm text-gray-200">
               Browse Categories
             </span>
           </Link>
