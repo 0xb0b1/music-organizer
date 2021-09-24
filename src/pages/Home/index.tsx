ca999513b3b8f53723ec6a5558eaaf15fe04ced7
Paulo Vicente <vcente82@gmail.com>
2021-09-24 10:45:53 -0300

    Home page to show recent played songs, and recommended

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import { Categories } from "../../components/Categories";
+import { ShowPlayer } from "../../components/ShowPlayer";
+import { Sidebar } from "../../components/Sidebar";
+import { UserHome } from "../../components/UserHome";
+
+export const Home = () => {
+  return (
+    <div className="flex h-home bg-gray-900">
+      <Sidebar />
+      <UserHome />
+      <ShowPlayer />
+    </div>
+  );
+};
